041cda61d4aafe0c17841c997f6f2f074a6e7252
Gia Thinh Nguyen <gthinh.nguyen.dev@gmail.com>
2024-05-22 22:45:22 -0400

    add mdx and expressiveCode for basic blog pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from "astro/config";
-
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import expressiveCode from "astro-expressive-code";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), expressiveCode(), mdx()],
 });
